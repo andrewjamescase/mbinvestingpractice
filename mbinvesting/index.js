0504c43213c12980163c7ecf7cfcc9d90842ba7d
andrewcasedayton <andrew.case801@gmail.com>
2019-09-18 12:03:10 -0400

    first successful iex api call. Displays latest apple information on the /financial route

--- mbinvesting/index.js
+++ mbinvesting/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const fetch = require("node-fetch");
 const { pool } = require('./config')
 
 const app = express()
@@ -19,6 +20,30 @@ const getChart = (request, response) => {
     })
 }
 
+app.get('/financial', (req, res) => {
+    //build api URL with user zip
+    const baseUrl = 'https://cloud.iexapis.com';
+    //ENTER YOUR API KEY HERE (make sure to no include < >)
+    const apiId = '?token=pk_10651120478d4b44a4f431c33e678d40';
+    const path = '/stable/stock/aapl/quote';
+
+    const userLocation = (url1, path, key) => {
+       let newUrl = url1 + path + key;
+       return newUrl;
+    };
+
+    const apiUrl = userLocation(baseUrl, path, apiId);
+
+    fetch(apiUrl) 
+     .then(res => res.json())
+     .then(data => {
+       res.send({ data });
+     })
+    //res.status(200).json()
+    .catch(err => {
+       res.redirect('/error');
+    });
+ })
 
 app
     .route('/chart')
@@ -27,6 +52,9 @@ app
     app.get('/working', function(req, res) {
     res.send('Working page');
 })
+    app.get('/error', function(req, res) {
+        res.send('IEX request did not work');
+    })
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server listening')
