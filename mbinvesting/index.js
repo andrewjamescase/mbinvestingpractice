b1e90aef6c7ce852c1ec3134c654452d34df68ec
andrewcasedayton <andrew.case801@gmail.com>
2019-09-26 16:09:35 -0400

    Updated get requests to post to company and quote tables based on new data diagram

--- mbinvesting/index.js
+++ mbinvesting/index.js
@@ -1,17 +1,17 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const fetch = require("node-fetch");
+const fetch = require("node-fetch")
 const { pool } = require('./config')
 
 const app = express()
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 
 const getChart = (request, response) => {
-    pool.query("SELECT * FROM chart", (error, results) => {
+    pool.query("SELECT * FROM quoteiex", (error, results) => {
         if (error) {
             throw error
         }
@@ -19,51 +19,97 @@ const getChart = (request, response) => {
         response.status(200).json(results.rows)
     })
 }
-
-app.get('/financial', (req, res) => {
-    
+const getCompany = (request, response) => {
+    pool.query("SELECT * FROM companyiex", (error, results) => {
+        if (error) {
+            throw error
+        }
+        //console.log('here I am')
+        response.status(200).json(results.rows)
+    })
+}
+app.get('/financial/companyiex', (req, res) => {
     const baseUrl = 'https://cloud.iexapis.com';
-    
     const apiId = '?token=pk_10651120478d4b44a4f431c33e678d40';
-    const path = '/stable/stock/aapl/quote';
-    
+    const companyIEXpath = '/stable/stock/aapl/company';
     const userLocation = (url1, path, key) => {
-       let newUrl = url1 + path + key;
-       return newUrl;
+        let newUrl = url1 + path + key;
+        return newUrl;
     };
+    const companyapiUrl = userLocation(baseUrl, companyIEXpath, apiId);
+    console.log(companyapiUrl);
 
-    const apiUrl = userLocation(baseUrl, path, apiId);
-    console.log(apiUrl);
-    fetch(apiUrl) 
-     .then(res => res.json())
-     
-     .then(res => {
-        pool.query(`INSERT INTO chart(id, companysymbol, date, open, close, high, low, change, changepercent, volume, sector) VALUES(124, '${res.symbol}', '2019-09-19', ${res.open},${res.close},${res.high},${res.low},${res.change},${res.changePercent}, ${res.volume},'${res.primaryExchange}') `, (error, results) => {
-            if(error) {
-                console.log(error);
-                throw error
-            }
-            console.log("IEX successfully connected")
+    fetch(companyapiUrl)
+        .then(res => res.json())
+        .then(res => {
+            pool.query(`INSERT INTO companyiex(symbol, companyName, exchange, industry, sector, ceo, securityName) VALUES('${res.symbol}', '${res.companyName}', '${res.exchange}','${res.industry}', '${res.sector}', '${res.ceo}', '${res.securityName}')`, (error, results) => {
+                if (error) {
+                   console.log(error);
+                    throw error
+                }
+                console.log("IEX successfully connected")
+                
+           })
         })
-     })
-    
-    .catch(err => {
-        //console.log(res)
-       res.redirect('/error');
-    });
-    res.send("IEX connected successfully and inserted the row")
- })
+        .catch(err => {
+            //console.log(res)
+            res.redirect('/error');
+        });
+    res.send("IEX connected successfully and inserted the row for the companyiex table")
+})
+app.get('/financial/quoteiex', (req, res) => {
+    const symbolList = new Array ('appl', 'msft', 'amzn')
+    const baseUrl = 'https://cloud.iexapis.com';
+
+    const apiId = '?token=pk_10651120478d4b44a4f431c33e678d40';
+
+
+    symbolList.forEach(function(symbol)  {
+
+
+        const quoteIEXpath = `'/stable/stock/${symbol}/quote`;
+
+
+        const userLocation = (url1, path, key) => {
+            let newUrl = url1 + path + key;
+            return newUrl;
+        };
+        const quoteapiUrl = userLocation(baseUrl, quoteIEXpath, apiId);
+        console.log(quoteapiUrl);
+        fetch(quoteapiUrl)
+            .then(res => res.json())
+
+            .then(res => {
+                pool.query(`INSERT INTO quoteiex(symbol, open, close, previousClose, high, low, volume, change, changePercent, latestPrice, week52high, week52low, latestSource, lastUpdated) VALUES('${res.symbol}', ${res.open},${res.close},${res.previousClose},${res.high},${res.low},${res.volume}, ${res.change},${res.changePercent}, ${res.latestPrice}, ${res.week52High}, ${res.week52Low}, '${res.latestSource}', '${res.latestTime}') `, (error, results) => {
+                    if (error) {
+                        console.log(error);
+                        throw error
+                    }
+                    //console.log("IEX successfully connected")
+                })
+            })
+            .catch(err => {
+                //console.log(res)
+                res.redirect('/error'); 
+            });
+        res.send("IEX connected successfully and inserted the row for the quoteiex table")
+    })
+
+})
 
 app
     .route('/chart')
     .get(getChart);
-
-    app.get('/working', function(req, res) {
+app
+    .route('/company')
+    .get(getCompany);
+app.get('/working', function (req, res) {
     res.send('Working page');
 })
-    app.get('/error', function(req, res) {
-        res.send('IEX request did not work');
-    })
+app.get('/error', function (req, res) {
+    res.send('IEX request did not work');
+})
+
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server listening')
---
+++ mbinvesting/sql/initialCreate.sql
@@ -0,0 +1,166 @@
+CREATE TABLE CompanyIEX
+(
+    symbol varchar(6),
+    companyName varchar(255),
+    exchange varchar (20),
+    industry varchar (255),
+    sector varchar (255),
+    ceo varchar (100),
+    securityName varchar (255)
+);
+
+CREATE TABLE KeyStatsIEX
+(
+    symbol varchar(6),
+    companyName varchar(255),
+    marketcap bigint,
+    week52high float,
+    week52low float,
+    week52change float,
+    sharesOutstanding bigint,
+    "float" bigint,
+    avg10Volume bigint,
+    avg30Volume bigint,
+    day200MovingAvg float,
+    day50MovingAvg float,
+    employees bigint,
+    ttmEPS float,
+    ttmDividendRate float,
+    dividendYield float,
+    nextDividendDate date,
+    exDividendDate date,
+    nextEarningsDate date,
+    peRatio float,
+    beta float,
+    maxChangePercent float,
+    year5ChangePercent float,
+    year2ChangePercent float,
+    year1ChangePercent float,
+    ytdChangePercent float,
+    month6ChangePercent float,
+    month3ChangePercent float
+);
+
+CREATE TABLE IncomeStatementIEX
+(
+    symbol varchar(6),
+    reportDate date,
+    totalRevenue bigint,
+    costOfRevenue bigint,
+    grossProfit bigint,
+    researchAndDevelopment bigint,
+    sellingGeneralandAdmin bigint,
+    operatingExpense bigint,
+    operatingIncome bigint,
+    otherIncomeExpenseNet bigint,
+    ebit bigint,
+    interestIncome bigint,
+    pretaxIncome bigint,
+    incomeTax bigint,
+    minorityInterest bigint,
+    netIncome bigint,
+    netIncomeBasic bigint
+);
+
+CREATE TABLE QuoteIEX
+(
+    symbol varchar(6),
+    "open" float,
+    "close" float,
+    previousClose float,
+    high float,
+    low float,
+    volume bigint,
+    change float,
+    changePercent float,
+    latestPrice float,
+    week52high float,
+    week52low float,
+    latestSource varchar(255),
+    lastUpdated varchar(255)
+);
+
+CREATE TABLE EarningsIEX
+(
+    symbol varchar(6),
+    EPSreportDate date,
+    actualEPS float,
+    consensusEPS float,
+    EPSSurpriseDollar float,
+    fiscalPeriod varchar(255),
+    fiscalEndDate date,
+    yearAgo float,
+    yearAgoChangePercent float
+);
+
+CREATE TABLE CashFlowIEX
+(
+    symbol varchar(6),
+    reportDate date,
+    netIncome bigint,
+    depreciation bigint,
+    changesInReceivables bigint,
+    changesInInventories bigint,
+    cashChange bigint,
+    cashFlow bigint,
+    capitalExpenditures bigint,
+    investments bigint,
+    investingActivityOther bigint,
+    dividendsPaid bigint,
+    netBorrowings bigint,
+    otherFinancingCashFLows bigint,
+    cashFlowFInancing bigint,
+    exchangeRateEffect bigint
+);
+
+CREATE TABLE BalanceSheetIEX
+(
+    symbol varchar(6),
+    reportDate date,
+    currentCash bigint,
+    shortTermInvestments bigint,
+    receivables bigint,
+    inventory bigint,
+    otherCurrentAssets bigint,
+    currentAssets bigint,
+    longTermInvestments bigint,
+    propertyPlantEquipment bigint,
+    goodwill bigint,
+    intangibleAssets bigint,
+    otherAssets bigint,
+    totalAssets bigint,
+    accountsPayable bigint,
+    currentLongTermDebt bigint,
+    otherCurrentLiabilities bigint,
+    totalCurrentLiabilities bigint,
+    longTermDebt bigint,
+    otherLiabilities bigint,
+    minorityInterest bigint,
+    totalLiabilities bigint,
+    commonStock bigint,
+    retainedEarnings bigint,
+    treasuryStock bigint,
+    capitalSurplus bigint,
+    shareholderEquity bigint,
+    netTangibleAssets bigint
+);
+
+CREATE TABLE WeeklyAV
+(
+    symbol varchar(6),
+    priceDate date,
+    "open" float,
+    "close" float,
+    high float,
+    low float,
+    volume bigint
+);
+
+CREATE TABLE AveragesAV
+(
+    symbol varchar(6),
+    avgType varchar(10),
+    avgDate date,
+    avgValue float,
+    TimeInterval varchar(255)
+);
